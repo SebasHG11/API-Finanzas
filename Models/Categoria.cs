58187b8df1c4b8cc195358d5903cfb60e27d9421
SebasHG11 <sebashiguitag@gmail.com>
2024-08-19 16:16:01 -0500

    Creo modelos iniciales para la API

---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+namespace ApiFinanzas.Models {
+    public enum TipoCategoria {
+        Ingreso,
+        Gasto
+    }
+    public class Categoria {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public TipoCategoria Tipo { get; set; }
+        public decimal Presupuesto { get; set; }
+        public List<Ingreso> Ingresos { get; set; }
+        public List<Gasto> Gastos { get; set; }
+
+        public Categoria()
+        {
+            Ingresos = new List<Ingreso>();
+            Gastos = new List<Gasto>();
+        }
+    }
+}
---
+++ Models/Ingreso.cs
@@ -0,0 +1,10 @@
+namespace ApiFinanzas.Models {
+    public class Ingreso {
+        public int Id { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
